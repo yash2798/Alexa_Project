fd0b84250098321f1faec6ec90a1183e25a34808
Yash Raj Singh <yash.raj270998@gmail.com>
2018-06-11 06:31:23 +0530

    Some packages added and index.js modified

--- index.js
+++ index.js
@@ -1,69 +1,209 @@
-exports.handler = function(event, context){
-  const request = event.request;
-  //Type of Request!!
-  if (request.type === 'LaunchRequest'){
-    var resObj = {
-      outputText: 'Welcome to the online Shopper Skill. You can use me to shop online on a number of shopping sites. What are you looking for?',
-      repromptText: 'You can say for example I want to buy shoes from Amazon.',
-      endSession: false
-    };
-    response
+// This is template files for developing Alexa skills
 
+'use strict';
 
-  }
-  //code to be filled here for all three intents regarding data extraction through API
-  else if (request.type === 'IntentRequest'){
+var winston = require('winston');
 
-  }
-  else if (request.type === 'SessionEndedRequest'){
+var logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({ prettyPrint: true, timestamp: true, json: false, stderrLevels:['error']})
+    ]
+  });
 
-  }
-  else {
+var intentHandlers = {};
 
-  }
+if(process.env.NODE_DEBUG_EN) {
+  logger.level = 'debug';
 }
 
 
+exports.handler = function (event, context) {
+    try {
+
+        logger.info('event.session.application.applicationId=' + event.session.application.applicationId);
+
+        if (APP_ID !== '' && event.session.application.applicationId !== APP_ID) {
+            context.fail('Invalid Application ID');
+         }
+      
+        if (!event.session.attributes) {
+            event.session.attributes = {};
+        }
+
+        logger.debug('Incoming request:\n', JSON.stringify(event,null,2));
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
 
 
-//Function to return a response in the form of JSON.
-function response(resObj){
-   var res = {
-     version: "1.0",
-     response: {
-       outputSpeech: {
-         type: "PlainText",
-         text: resObj.ouputText
-    },
-      /*"card": {
-        "type": "Standard",
-        "title": "Title of the card",
-        "content": "Content of a simple card",
-        "text": "Text content for a standard card",
-        "image": {
-          "smallImageUrl": "https://url-to-small-card-image...",
-          "largeImageUrl": "https://url-to-large-card-image..."
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request, event.session, new Response(context,event.session));
+        } else if (event.request.type === 'IntentRequest') {
+            var response =  new Response(context,event.session);
+            if (event.request.intent.name in intentHandlers) {
+              intentHandlers[event.request.intent.name](event.request, event.session, response,getSlots(event.request));
+            } else {
+              response.speechText = 'Unknown intent';
+              response.shouldEndSession = true;
+              response.done();
+            }
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
         }
-      },*/
-       reprompt: {
-         outputSpeech: {
-           type: "PlainText",
-           text: ""
-        }
-      },
-      /*"directives": [
-        {
-          "type": "InterfaceName.Directive"
-          (...properties depend on the directive type)
-        }
-      ],*/
-    shouldEndSession: resObj.endSession
+    } catch (e) {
+        context.fail('Exception: ' + getError(e));
+    }
+};
+
+function getSlots(req) {
+  var slots = {}
+  for(var key in req.intent.slots) {
+    slots[key] = req.intent.slots[key].value;
+  }
+  return slots;
+}
+
+var Response = function (context,session) {
+  this.speechText = '';
+  this.shouldEndSession = true;
+  this.ssmlEn = true;
+  this._context = context;
+  this._session = session;
+
+  this.done = function(options) {
+
+    if(options && options.speechText) {
+      this.speechText = options.speechText;
+    }
+
+    if(options && options.repromptText) {
+      this.repromptText = options.repromptText;
+    }
+
+    if(options && options.ssmlEn) {
+      this.ssmlEn = options.ssmlEn;
+    }
+
+    if(options && options.shouldEndSession) {
+      this.shouldEndSession = options.shouldEndSession;
+    }
+
+    this._context.succeed(buildAlexaResponse(this));
+  }
+
+  this.fail = function(msg) {
+    logger.error(msg);
+    this._context.fail(msg);
+  }
+
+};
+
+function createSpeechObject(text,ssmlEn) {
+  if(ssmlEn) {
+    return {
+      type: 'SSML',
+      ssml: '<speak>'+text+'</speak>'
+    }
+  } else {
+    return {
+      type: 'PlainText',
+      text: text
+    }
+  }
+}
+
+function buildAlexaResponse(response) {
+  var alexaResponse = {
+    version: '1.0',
+    response: {
+      outputSpeech: createSpeechObject(response.speechText,response.ssmlEn),
+      shouldEndSession: response.shouldEndSession
     }
   };
-  if (resObj.repromptText){
-    res.response.reprompt.outputSpeech.text = resObj.repromptText;
+
+  if(response.repromptText) {
+    alexaResponse.response.reprompt = {
+      outputSpeech: createSpeechObject(response.repromptText,response.ssmlEn)
+    };
   }
-  return res;
+
+  if(response.cardTitle) {
+    alexaResponse.response.card = {
+      type: 'Simple',
+      title: response.cardTitle
+    };
+
+    if(response.imageUrl) {
+      alexaResponse.response.card.type = 'Standard';
+      alexaResponse.response.card.text = response.cardContent;
+      alexaResponse.response.card.image = {
+        smallImageUrl: response.imageUrl,
+        largeImageUrl: response.imageUrl
+      };
+    } else {
+      alexaResponse.response.card.content = response.cardContent;
+    }
+  }
+
+  if (!response.shouldEndSession && response._session && response._session.attributes) {
+    alexaResponse.sessionAttributes = response._session.attributes;
+  }
+  logger.debug('Final response:\n', JSON.stringify(alexaResponse,null,2),'\n\n');
+  return alexaResponse;
 }
 
-//yashSS
+function getError(err) {
+  var msg='';
+  if (typeof err === 'object') {
+    if (err.message) {
+      msg = ': Message : ' + err.message;
+    }
+    if (err.stack) {
+      msg += '\nStacktrace:';
+      msg += '\n====================\n';
+      msg += err.stack;
+    }
+  } else {
+    msg = err;
+    msg += ' - This error is not object';
+  }
+  return msg;
+}
+
+
+//--------------------------------------------- Skill specific logic starts here ----------------------------------------- 
+
+//Add your skill application ID from amazon devloper portal
+var APP_ID = '';
+
+function onSessionStarted(sessionStartedRequest, session) {
+    logger.debug('onSessionStarted requestId=' + sessionStartedRequest.requestId + ', sessionId=' + session.sessionId);
+    // add any session init logic here
+    
+}
+
+function onSessionEnded(sessionEndedRequest, session) {
+  logger.debug('onSessionEnded requestId=' + sessionEndedRequest.requestId + ', sessionId=' + session.sessionId);
+  // Add any cleanup logic here
+  
+}
+
+function onLaunch(launchRequest, session, response) {
+  logger.debug('onLaunch requestId=' + launchRequest.requestId + ', sessionId=' + session.sessionId);
+
+  //response.speechText = 'Welcome msg';
+  //response.repromptText = 'Reprompt msg';
+  response.shouldEndSession = false;
+  response.done();
+}
+
+
+/** For each intent write a intentHandlers
+Example:
+intentHandlers['HelloIntent'] = function(request,session,response,slots) {
+  //Intent logic
+  
+}
+**/
